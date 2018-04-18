dbac57864777e68bbdd43beef9a18b2e1ced18c2
wk3368 <wk3368@163.com>
2018-04-18 22:31:49 +0800

    发布脚本优化，支持.gitbook的替换

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,8 @@ def deploy():
     local('[ -d "_book" ] && rm -rf _book')
     local("gitbook build")
     local("cp ./_book/.gitbook/assets/* ./_book/assets/")
-    local('find . -name "*.html" |xargs sed -i "s#.gitbook##g"')
+    #local('sed -i 's/\.gitbook\///g' ./_book/**/*.html')  # 连续两个*在命令行可以，但是这里运行无效
+    local('find . -name "_book/*.html" |xargs sed -i "s#.gitbook/##g"')
     run('[ -d "/tmp/_book" ] && rm -r /tmp/_book/*')
     put("_book/", "/tmp")
     run('[ -d "/tmp/_back" ] && rm -r /tmp/_back/*')
