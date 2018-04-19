6f9ead80561806132962ae4bc2c661876e77a0a7
Shuo Wang <shuowang@growingio.com>
2018-04-19 18:02:53 +0800

    revert fabfile.py to old version and mv API to api_old

--- fabfile.py
+++ fabfile.py
@@ -4,15 +4,11 @@ from fabric.api import *
 
 env.hosts = "apps@cnprd3"
 
+
 @task
 def deploy():
     local("git pull")
-    local('[ -d "_book" ] && rm -rf _book')
     local("gitbook build")
-    local("cp ./_book/.gitbook/assets/* ./_book/assets/")
-    local("rm -rf ./_book/.gitbook")
-    #local('sed -i 's/\.gitbook\///g' ./_book/**/*.html')  # 连续两个*在命令行可以，但是这里运行无效
-    local('find . -name "*.html" |xargs sed -i "s#\.gitbook/##g"')
     run('[ -d "/tmp/_book" ] && rm -r /tmp/_book/*')
     put("_book/", "/tmp")
     run('[ -d "/tmp/_back" ] && rm -r /tmp/_back/*')
